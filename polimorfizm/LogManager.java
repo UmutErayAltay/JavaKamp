e44c0c9091f3600ce452dc7cdd02fcc37cbf2846
UmutErayAltay <83500869+UmutErayAltay@users.noreply.github.com>
2021-05-08 18:06:45 +0300

    Add files via upload

---
+++ polimorfizm/LogManager.java
@@ -0,0 +1,11 @@
+package polimorfizm;
+
+public class LogManager {
+	public void log() {
+		
+	}
+} 
+
+//1 - Database
+//2 - File
+//3 - Email
---
+++ polimorfizm/Main.java
@@ -0,0 +1,15 @@
+package polimorfizm;
+
+public class Main {
+
+	public static void main(String[] args) {
+		BaseLogger[] loggers = new BaseLogger[] {new DatabaseLogger(), new EmailLogger(), new FileLogger(), new ConsoleLogger()};
+		for (BaseLogger logger : loggers) {
+			logger.log("log mesajý");
+		}
+		
+		CustomerManager customerManager = new CustomerManager(new FileLogger());
+		customerManager.add();
+	}
+
+}
